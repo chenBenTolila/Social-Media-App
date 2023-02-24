a20ee07c1f9f79fa61c66502b9465eea2bdec7c0
chenBenTolila <chenbt266@google.com>
2023-02-24 11:33:45 +0200

    adding files

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,8 @@ const db = mongoose.connection
 db.on('error',error=>{console.error(error)})
 db.once('open',()=>{console.log('connected to mongo DB')})
 
-app.use('/public',express.static('public'))
+app.use('/public', express.static('public'))
+app.use('/uploads', express.static('uploads'))
  
 import authRouter from './routes/auth_route.js'
 app.use('/auth',authRouter)
@@ -30,6 +31,11 @@ app.use('/auth',authRouter)
 import postRouter from './routes/post_route.js'
 app.use('/post',postRouter)
 
+import studentRouter from './routes/student_route.js'
+app.use('/student', studentRouter)
+
+import fileRouter from './routes/file_route.js'
+app.use('/file', fileRouter)
 
 import swaggerUI from "swagger-ui-express"
 import swaggerJsDoc from "swagger-jsdoc"
